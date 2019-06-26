54e88f058d6a30d6c548c387139150c65ec7aeaa
trista-paul <trista-paul-linux@LAPTOP-MEEM5KMV.localdomain>
2019-06-26 02:32:43 -0700

    heres the fun part ... figuring out the format of the key when all that comes up are StackOverflow answers with dummy values

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ server = app.server
 connection = S3Connection(os.environ['awskey'], os.environ['awsSkey'])
 bucket = connection.get_bucket('lobby-data', validate=False)
 temp = '/tmp/lobby_model3.joblib'
-bucket.get_key(aws_app_assets + 'lobby_model3.joblib').get_contents_to_filename(temp)
+bucket.get_key('lobby_model3.joblib').get_contents_to_filename(temp)
 model = joblib.load(temp)
 os.remove(temp)
 
